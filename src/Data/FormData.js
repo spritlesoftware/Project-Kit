1396e1f1b70f2530875e651db55763981339bc03
Janani-Gopi <jananirave19@gmail.com>
2023-11-01 20:53:58 +0530

    feat:forms-text,radio

---
+++ src/Data/FormData.js
@@ -0,0 +1,44 @@
+
+export const formData =[
+      {
+        "id": "firstName",
+        "label": "First Name",
+        "type": "text",
+        "required": true
+      },
+      {
+        "id": "lastName",
+        "label": "Last Name",
+        "type": "text",
+        "required": true
+      },
+      {
+        "id": "email",
+        "label": "Email",
+        "type": "email",
+        "required": true
+      },
+      {
+        "id": "gender",
+        "label": "Gender",
+        "type": "select",
+        "options": ["Male", "Female", "Other"],
+        "required": true
+      },
+      {
+        "id": "subscription",
+        "label": "Newsletter Subscription",
+        "type": "radio",
+        "options": ["Yes", "No"],
+        "required": false
+      },
+      {
+        "id": "interests",
+        "label": "Interests",
+        "type": "checkbox",
+        "options": ["Sports", "Music", "Movies", "Books"],
+        "required": false
+      }
+    ]
+  
+  
