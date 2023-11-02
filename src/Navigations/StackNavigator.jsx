f63456b570bc98195ac2b9d01e9e833125f21020
gowrishankar <gowrishankaroffl@gmail.com>
2023-11-02 12:20:36 +0530

    Stack navigator setuped

--- src/Navigations/StackNavigator.jsx
+++ src/Navigations/StackNavigator.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from '../Containers/Authentications/Login';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "../Containers/Authentications/Login";
+import Register from "../Containers/Authentications/Register";
 
 const Stack = createNativeStackNavigator();
 
@@ -8,15 +9,15 @@ const StackNavigator = () => {
   return (
     <Stack.Navigator
       screenOptions={{
-          headerShown: false,
-          // presentation: 'modal',
-          animationTypeForReplace: 'push',
-          animation: 'slide_from_right',
-        }}
-    >
-        <Stack.Screen name="Login" component={Login} />
-      </Stack.Navigator>
-  )
-}
+        headerShown: false,
+        // presentation: 'modal',
+        animationTypeForReplace: "push",
+        animation: "slide_from_right",
+      }}>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="SignUp" component={Register} />
+    </Stack.Navigator>
+  );
+};
 
-export default StackNavigator
+export default StackNavigator;
