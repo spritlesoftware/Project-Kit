d8b9f580cb72d3092293d488e79f7a20622fbe6b
gowrishankar <gowrishankaroffl@gmail.com>
2023-11-02 12:19:59 +0530

    Login UI with minimal logic

--- src/Containers/Authentications/Login.jsx
+++ src/Containers/Authentications/Login.jsx
@@ -1,16 +1,114 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { styles } from './LoginStyles'
-import LoginLogo from '../../Assets/images/login_logo.svg';
+import { TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { styles } from "./LoginStyles";
+import LoginLogo from "../../Assets/images/login_logo.svg";
+import Google from "../../Assets/images/google.svg";
+import Facebook from "../../Assets/images/facebook.svg";
+import CustomButton from "../../Components/Button/CustomButton";
+import InputField from "../../Components/TextInput/InputField";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { moderateScale } from "react-native-size-matters";
+import { SafeAreaView } from "react-native-safe-area-context";
+// import Google from 'react-native-vector-icons/AntDesign';
+import { colors } from "../../Utils/colors";
+import { Divider, Text } from "react-native-paper";
+import SocialButton from "../../Components/Button/SocialButton";
+import { fonts } from "../../Utils/fonts";
 
-const Login = () => {
+const Login = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [invalidEmail, setInvalidEmail] = useState(false);
+  const [invalidPassword, setInvalidPassword] = useState(false);
+  const [apiError, setApiError] = useState(false);
+  const [apiErrorMessage, setApiErrorMessage] = useState("");
+
+  // custom user login logic
+  const userLogin = () => {};
+
+  // handling signin
+  const onPressSignin = async () => {
+    if (!email.trim()) {
+      setInvalidEmail(true);
+      return;
+    } else if (!password.trim()) {
+      setInvalidEmail(false);
+      setInvalidPassword(true);
+      return;
+    } else {
+      setInvalidEmail(false);
+      setInvalidPassword(false);
+      userLogin();
+    }
+  };
+
+  console.log(email, password);
+
   return (
-    <View style={styles.center}>
-      <View style={{ flex: 1, alignItems: 'center' }}>
-        <LoginLogo width={300} height={300} />
-      </View>
-    </View>  
-  )
-}
+    <SafeAreaView style={styles.center}>
+      <KeyboardAwareScrollView
+        style={styles.keyboard}
+        keyboardShouldPersistTaps="handled">
+        <View style={{ alignItems: "center" }}>
+          <LoginLogo width={200} height={200} />
+        </View>
+        <View style={styles.titleContainer}>
+          <Text style={styles.headerTitle} variant="displayMedium">
+            Kit Box
+          </Text>
+          <Text style={styles.headerTitle} variant="headlineSmall">
+            Sign In
+          </Text>
+        </View>
+        <View style={styles.inputContainer}>
+          {!apiError ? (
+            <Text style={styles.apiErrorStyle}>{apiErrorMessage}</Text>
+          ) : null}
+          <InputField
+            label={"Username"}
+            placeholder={"Enter username"}
+            textContentType="emailAddress"
+            value={email}
+            onChangeText={(text) => {
+              setApiError(false);
+              setInvalidEmail(false);
+              setEmail(text);
+            }}
+            error={invalidEmail}
+          />
+          <InputField
+            label={"Password"}
+            placeholder={"Enter password"}
+            textContentType="password"
+            secureTextEntrys
+            value={password}
+            onChangeText={(text) => {
+              setApiError(false);
+              setInvalidPassword(false);
+              setPassword(text);
+            }}
+            error={invalidPassword}
+          />
+          <TouchableOpacity
+            style={styles.forgotPasswordContainer}
+            onPress={() => {}}>
+            <Text style={styles.forgotPassword}>Forgot your password?</Text>
+          </TouchableOpacity>
+          <CustomButton title={"Login"} onPress={onPressSignin} />
+        </View>
+        <TouchableOpacity
+          style={styles.regContainer}
+          onPress={() => navigation.navigate("SignUp")}>
+          <Text style={styles.regText}>Don't have an account? Register</Text>
+        </TouchableOpacity>
+        <Divider style={styles.divider} />
+        <Text style={styles.orContainer}>Sign in with social account</Text>
+        <View style={styles.socialContainer}>
+          <SocialButton icon={<Google width={30} height={30} />} />
+        </View>
+      </KeyboardAwareScrollView>
+    </SafeAreaView>
+  );
+};
 
-export default Login
+export default Login;
