ff51610627fc0032a41f2cb6f523ec585e37bc2f
gowrishankar <gowrishankaroffl@gmail.com>
2023-11-06 18:25:10 +0530

    Dummy datas for developing

--- src/Data/Users.js
+++ src/Data/Users.js
@@ -40,3 +40,54 @@ export const Users = [
     name: 'Daniel Wilson',
   },
 ];
+    {
+      "user_id": 1,
+      "name": "Alice Johnson",
+      "last_msg": "Hi there!",
+      "modified_date": "2023-11-03"
+    },
+    {
+      "user_id": 2,
+      "name": "Bob Smith",
+      "last_msg": "Good morning!",
+      "modified_date": "2023-11-03"
+    },
+    {
+      "user_id": 3,
+      "name": "Eva Williams",
+      "last_msg": "How's it going?",
+      "modified_date": "2023-11-02"
+    },
+    {
+      "user_id": 4,
+      "name": "David Brown",
+      "last_msg": "See you later!",
+      "modified_date": "2023-11-03"
+    },
+    {
+      "user_id": 5,
+      "name": "Olivia Davis",
+      "last_msg": "I'm on my way!",
+      "modified_date": "2023-11-03"
+    },
+    {
+      "user_id": 6,
+      "name": "Michael Lee",
+    },
+    {
+      "user_id": 7,
+      "name": "Sophia White",
+    },
+    {
+      "user_id": 8,
+      "name": "James Miller",
+    },
+    {
+      "user_id": 9,
+      "name": "Emma Harris",
+    },
+    {
+      "user_id": 10,
+      "name": "Daniel Wilson",
+    }
+  ]
