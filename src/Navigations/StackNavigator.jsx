e9bb7c81244a8befbaef28f570f96039f2dd9227
gowrishankar <gowrishankaroffl@gmail.com>
2023-11-06 18:25:30 +0530

    Added chatroom and list screens

--- src/Navigations/StackNavigator.jsx
+++ src/Navigations/StackNavigator.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, {useState} from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Login from '../Containers/Authentications/Login';
 import Register from '../Containers/Authentications/Register';
 import ForgotPassword from '../Containers/Authentications/ForgotPassword';
 import Logout from '../Containers/Authentications/Logout';
 import Form from '../Containers/Form';
+import RealChat from '../Containers/Chats/RealChat';
 import ChatList from '../Containers/Chats/ChatList';
-import { Videocall } from '../Containers/Videocall/Videocall';
-import { RegisterScreen } from '../Containers/Videocall/RegisterScreen';
+import {Videocall} from '../Containers/Videocall/Videocall';
+import {RegisterScreen} from '../Containers/Videocall/RegisterScreen';
 import {
   StyleSheet,
   View,
@@ -22,17 +23,16 @@ import {
   Dimensions,
 } from 'react-native';
 import NewItem from '../Containers/Table/NewItem';
-import { TableData } from '../Data/TableData';
-import Table from '../Containers/Table/Table'
+import {TableData} from '../Data/TableData';
+import Table from '../Containers/Table/Table';
 import EditItem from '../Containers/Table/EditItem';
 import GroupList from '../Containers/Chats/GroupList';
 import Contacts from '../Containers/Chats/Contacts';
 
-
 export const initialState = {
   isAudioEnabled: true,
   status: 'disconnected',
-  participants:[],
+  participants: [],
   videoTracks: new Map(),
   userName: '',
   roomName: '',
@@ -41,42 +41,37 @@ export const initialState = {
 
 export const AppContext = React.createContext(initialState);
 
-const dimensions = Dimensions.get('window');
 const Stack = createNativeStackNavigator();
-
-export const AppContext = React.createContext();
 
 const StackNavigator = () => {
   const [props, setProps] = useState(initialState);
-   const [tableData, setTableData] = React.useState(TableData)
+  const [tableData, setTableData] = React.useState(TableData);
 
   return (
-    <AppContext.Provider value={{tableData,setTableData,props, setProps}}>
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-        // presentation: 'modal',
-        animationTypeForReplace: 'push',
-        animation: 'slide_from_right',
-      }}>
-       <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-      <Stack.Screen name="Videocall" component={Videocall} />
-      <Stack.Screen name="Table" component={Table} />
-      <Stack.Screen name="NewItem" component={NewItem} />
-      <Stack.Screen name="EditItem" component={EditItem} />
-      <Stack.Screen name="Form" component={Form} />
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="SignUp" component={Register} />
-      <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-      <Stack.Screen name="Logout" component={Logout} />
-      <Stack.Screen name="ChatList" component={ChatList} />
-      <Stack.Screen name="Groups" component={GroupList} />
-      <Stack.Screen name="Contacts" component={Contacts} />
-    </Stack.Navigator>
+    <AppContext.Provider value={{tableData, setTableData, props, setProps}}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          // presentation: 'modal',
+          animationTypeForReplace: 'push',
+          animation: 'slide_from_right',
+        }}>
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+        <Stack.Screen name="Videocall" component={Videocall} />
+        <Stack.Screen name="Table" component={Table} />
+        <Stack.Screen name="NewItem" component={NewItem} />
+        <Stack.Screen name="EditItem" component={EditItem} />
+        <Stack.Screen name="Form" component={Form} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SignUp" component={Register} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+        <Stack.Screen name="Logout" component={Logout} />
+        <Stack.Screen name="ChatList" component={ChatList} />
+        <Stack.Screen name="Groups" component={GroupList} />
+        <Stack.Screen name="Contacts" component={Contacts} />
+      </Stack.Navigator>
     </AppContext.Provider>
-
   );
-  
 };
 
 export default StackNavigator;
