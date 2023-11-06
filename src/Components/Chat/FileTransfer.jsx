ee9d790b9e9afa1c1fd1d5a251d159a8ad36f39f
gowrishankar <gowrishankaroffl@gmail.com>
2023-11-06 18:26:41 +0530

    FIle sharing logic code and UI

---
+++ src/Components/Chat/FileTransfer.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+
+const FileTransfer = ({filePath}) => {
+  var fileType = '';
+  var name = '';
+  if (filePath !== undefined) {
+    name = filePath.split('/').pop();
+    fileType = filePath.split('.').pop();
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.frame}>
+        <Image
+          source={
+            fileType === 'pdf'
+              ? require('../../Assets/images/pdf.png')
+              : require('../../Assets/images/png.png')
+          }
+          style={{height: 60, width: 60}}
+        />
+        <View>
+          <Text style={styles.text}>
+            {name.replace('%20', '').replace(' ', '')}
+          </Text>
+          <Text style={styles.textType}>{fileType.toUpperCase()}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+export default FileTransfer;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 5,
+    borderRadius: 15,
+    padding: 5,
+  },
+  text: {
+    color: 'black',
+    marginTop: 10,
+    fontSize: 16,
+    lineHeight: 20,
+    marginLeft: 5,
+    marginRight: 5,
+  },
+  textType: {
+    color: 'black',
+    marginTop: 5,
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginLeft: 10,
+  },
+  frame: {
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    borderRadius: 10,
+    padding: 5,
+    marginTop: -4,
+  },
+});
