b95e7e05cf63b8e5e2bace608fce244e99252f8f
gowrishankar <gowrishankaroffl@gmail.com>
2023-11-10 15:25:11 +0530

    Updated the styles & conditional rending

--- src/Components/Chat/FileTransfer.jsx
+++ src/Components/Chat/FileTransfer.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import {View, Text, StyleSheet, Image} from 'react-native';
 import {moderateScale} from 'react-native-size-matters';
+import {colors} from '../../Utils/colors';
 
-const FileTransfer = ({filePath}) => {
+const FileTransfer = props => {
   var fileType = '';
   var name = '';
-  if (filePath !== undefined) {
-    name = filePath.split('/').pop();
-    fileType = filePath.split('.').pop();
+  if (props.filePath !== undefined) {
+    name = props.filePath.split('/').pop();
+    fileType = props.filePath.split('.').pop();
   }
 
   return (
-    <View style={styles.container}>
+    <View
+      style={[
+        styles.container,
+        {
+          marginBottom: props.isFooter && moderateScale(50),
+          minWidth: props.isFooter ? '' : moderateScale(200),
+        },
+      ]}>
       <View style={styles.frame}>
         <Image
           source={
@@ -21,10 +29,14 @@ const FileTransfer = ({filePath}) => {
           }
           style={{height: 60, width: 60}}
         />
-        <View>
-          <Text style={styles.text}>{name.replace(/%20| /g, '')}</Text>
-          <Text style={styles.textType}>{fileType.toUpperCase()}</Text>
-        </View>
+        {props.isFooter ? (
+          ''
+        ) : (
+          <View>
+            <Text style={styles.text}>{name.replace(/%20| /g, '')}</Text>
+            <Text style={styles.textType}>{fileType.toUpperCase()}</Text>
+          </View>
+        )}
       </View>
     </View>
   );
@@ -45,7 +57,6 @@ const styles = StyleSheet.create({
     lineHeight: 20,
     marginLeft: 5,
     marginRight: 5,
-    width: moderateScale(220),
   },
   textType: {
     color: 'black',
