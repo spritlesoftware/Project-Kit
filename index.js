03a90dbb71f23bd795708d7499a1606518a27b7b
gowrishankar <gowrishankaroffl@gmail.com>
2023-11-15 13:35:33 +0530

    Required dependencies added

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 /**
  * @format
  */
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import { PaperProvider } from 'react-native-paper';
+import {PaperProvider} from 'react-native-paper';
+import TrackPlayer from 'react-native-track-player';
+import {playbackService} from './src/Containers/AudioPlayer/AudioPlayerServices';
 
 export default function Main() {
-    return (
-      <PaperProvider>
-        <App />
-      </PaperProvider>
-    );
-  }
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+}
 
 AppRegistry.registerComponent(appName, () => Main);
+TrackPlayer.registerPlaybackService(() => playbackService);
