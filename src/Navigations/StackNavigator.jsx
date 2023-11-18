72bd3b16e19180c53a131bbf7c44df2ace2b34b7
Janani-Gopi <jananirave19@gmail.com>
2023-11-18 22:14:17 +0530

    feat:Editing feature with UI enhanced

--- src/Navigations/StackNavigator.jsx
+++ src/Navigations/StackNavigator.jsx
@@ -5,11 +5,19 @@ import Register from '../Containers/Authentications/Register';
 import ForgotPassword from '../Containers/Authentications/ForgotPassword';
 import Logout from '../Containers/Authentications/Logout';
 import ChatList from '../Containers/Chats/ChatList';
+import NewItem from '../Containers/Table/NewItem';
+import { TableData } from '../Data/TableData';
+import Table from '../Containers/Table/Table'
+import EditItem from '../Containers/Table/EditItem';
 
 const Stack = createNativeStackNavigator();
 
+export const AppContext = React.createContext();
+
 const StackNavigator = () => {
+  const [tableData, setTableData] = React.useState(TableData)
   return (
+    <AppContext.Provider value={{tableData,setTableData}}>
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
@@ -17,12 +25,16 @@ const StackNavigator = () => {
         animationTypeForReplace: 'push',
         animation: 'slide_from_right',
       }}>
+      <Stack.Screen name="Table" component={Table} />
+      <Stack.Screen name="NewItem" component={NewItem} />
+      <Stack.Screen name="EditItem" component={EditItem} />
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="SignUp" component={Register} />
       <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
       <Stack.Screen name="Logout" component={Logout} />
       <Stack.Screen name="ChatList" component={ChatList} />
     </Stack.Navigator>
+    </AppContext.Provider>
   );
 };
 
