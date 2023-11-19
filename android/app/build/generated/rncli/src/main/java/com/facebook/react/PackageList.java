9dd35f7f7ff6e35064456aa4e4e26771f40bc83c
Janani-Gopi <jananirave19@gmail.com>
2023-11-19 18:07:53 +0530

    conflicts resolved

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -11,16 +11,10 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-// react-native-gesture-handler
-import com.swmansion.gesturehandler.RNGestureHandlerPackage;
 // react-native-safe-area-context
 import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 // react-native-screens
 import com.swmansion.rnscreens.RNScreensPackage;
-// react-native-svg
-import com.horcrux.svg.SvgPackage;
-// react-native-vector-icons
-import com.oblador.vectoricons.VectorIconsPackage;
 
 public class PackageList {
   private Application application;
@@ -66,11 +60,8 @@ public class PackageList {
   public ArrayList<ReactPackage> getPackages() {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
       new MainReactPackage(mConfig),
-      new RNGestureHandlerPackage(),
       new SafeAreaContextPackage(),
-      new RNScreensPackage(),
-      new SvgPackage(),
-      new VectorIconsPackage()
+      new RNScreensPackage()
     ));
   }
 }
