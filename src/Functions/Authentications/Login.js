95d4c334e0560e9c248691950b3e8f0a85122237
gowrishankar <gowrishankaroffl@gmail.com>
2023-11-21 12:28:23 +0530

    Moved these files to specific folders

---
+++ src/Functions/Authentications/Login.js
@@ -0,0 +1,60 @@
+// LoginLogic.js
+import {useState} from 'react';
+
+const LoginLogic = navigation => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [invalidEmail, setInvalidEmail] = useState(false);
+  const [invalidPassword, setInvalidPassword] = useState(false);
+  const [apiError, setApiError] = useState(false);
+  const [apiErrorMessage, setApiErrorMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const userLogin = () => {
+    setIsLoading(false);
+    navigation.replace('Logout');
+  };
+
+  const onPressSignin = async () => {
+    if (!email.trim()) {
+      setInvalidEmail(true);
+      return;
+    } else if (!password.trim()) {
+      setInvalidEmail(false);
+      setInvalidPassword(true);
+      return;
+    } else {
+      setIsLoading(true);
+      setInvalidEmail(false);
+      setInvalidPassword(false);
+      userLogin();
+    }
+  };
+
+  const handleEmailChange = text => {
+    setApiError(false);
+    setInvalidEmail(false);
+    setEmail(text);
+  };
+
+  const handlePasswordChange = text => {
+    setApiError(false);
+    setInvalidPassword(false);
+    setPassword(text);
+  };
+
+  return {
+    email,
+    password,
+    invalidEmail,
+    invalidPassword,
+    apiError,
+    apiErrorMessage,
+    isLoading,
+    onPressSignin,
+    handleEmailChange,
+    handlePasswordChange,
+  };
+};
+
+export default LoginLogic;
