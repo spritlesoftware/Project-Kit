9bd720a63bda4a63aa50b3fe300cd04eb51ae3e5
Janani-Gopi <jananirave19@gmail.com>
2023-11-21 11:26:56 +0530

    styles:Fonts added

--- server.js
+++ server.js
@@ -39,35 +39,3 @@ app.listen(port, () =>
 ngrok.connect(port).then((url) => {
   console.log(`Server forwarded to public url ${url}`);
 });
-// const express = require('express');
-// const twilio = require('twilio');
-
-// const app = express();
-// const port = 3000;
-
-// // Twilio API key SID and secret from your Twilio account dashboard
-// const apiKeySid = 'SK72cdcfd21bb8d6e9554082444ae21624';
-// const apiKeySecret = 'hSTbkOGC4JDKBKMyt4hmCma9Gd5CHtPF';
-
-// // Middleware to authenticate the Twilio access token
-// app.use('/validate-token', (req, res, next) => {
-//   const token = req.query.token;
-
-//   try {
-//     const decodedToken = jwt.AccessToken.verify(token, apiKeySecret);
-//     req.identity = decodedToken.identity;
-//     next();
-//   } catch (error) {
-//     res.status(401).json({ error: 'Invalid token' });
-//   }
-// });
-
-// // Endpoint that requires authentication
-// app.get('/protected', (req, res) => {
-//   res.json({ message: `Hello, ${res.identity}! You are authenticated.` });
-// });
-
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server listening at http://localhost:${port}`);
-// });
