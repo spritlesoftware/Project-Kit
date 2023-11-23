f4d054574e8f659e9a61b761cdc01ed4cdcdf808
gowrishankar <gowrishankaroffl@gmail.com>
2023-11-23 21:26:18 +0530

    Update: Added Bottom player & audio status check

--- src/Containers/AudioPlayer/PlayListTracks.jsx
+++ src/Containers/AudioPlayer/PlayListTracks.jsx
@@ -1,4 +1,4 @@
-import {View, Text, FlatList, StyleSheet} from 'react-native';
+import {View, FlatList, StyleSheet} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import TracklList from '../../Components/Audio/TrackList';
 import TrackPlayer, {
@@ -8,11 +8,17 @@ import TrackPlayer, {
 } from 'react-native-track-player';
 import {colors} from '../../Utils/colors';
 import {moderateScale} from 'react-native-size-matters';
+import {fonts} from '../../Utils/fonts';
+import {Text} from 'react-native-paper';
+import Back from 'react-native-vector-icons/AntDesign';
+import Menu from 'react-native-vector-icons/Entypo';
+import PlayingTrackBottom from '../../Components/Audio/PlayingTrackBottom';
 
-const PlayListTracks = () => {
+const PlayListTracks = ({navigation}) => {
   const [queue, setQueue] = useState([]);
   const [currentTrack, setCurrentTrack] = useState([]);
   const [favourites, setFavourites] = useState([]);
+  const [audioStatus, setAudioStatus] = useState();
 
   async function loadPlaylist() {
     const queue = await TrackPlayer.getQueue();
@@ -49,8 +55,37 @@ const PlayListTracks = () => {
   function handleItemPress(index) {
     TrackPlayer.skip(index);
   }
+
+  async function handlePlayPress() {
+    if ((await TrackPlayer.getState()) == State.Playing) {
+      TrackPlayer.pause();
+      setAudioStatus('Pause');
+    } else {
+      TrackPlayer.play();
+      setAudioStatus('Playing');
+    }
+  }
+
   return (
     <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Back.Button
+          name="left"
+          color={colors.WHITE}
+          size={moderateScale(20)}
+          style={styles.button}
+          onPress={() => navigation.goBack()}
+        />
+        <Text variant="headlineSmall" style={styles.headerTitle}>
+          TRACKit
+        </Text>
+        <Menu.Button
+          name="dots-three-vertical"
+          color={colors.WHITE}
+          size={moderateScale(20)}
+          style={styles.button}
+        />
+      </View>
       <FlatList
         data={queue}
         renderItem={({item, index}) => (
@@ -59,9 +94,27 @@ const PlayListTracks = () => {
             data={item}
             handleItemPress={handleItemPress}
             favourites={favourites}
+            currentTrack={currentTrack}
+            audioStatus={audioStatus}
           />
         )}
       />
+      {currentTrack.length !== 0 && (
+        <View
+          style={{
+            justifyContent: 'center',
+            padding: moderateScale(10),
+          }}>
+          <PlayingTrackBottom
+            currentTrack={currentTrack}
+            favourites={favourites}
+            HandleFavourites={HandleFavourites}
+            handleItemPress={handleItemPress}
+            audioStatus={audioStatus}
+            handlePlayPress={handlePlayPress}
+          />
+        </View>
+      )}
     </View>
   );
 };
@@ -71,6 +124,26 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.BLACK,
     paddingVertical: moderateScale(10),
+  },
+
+  headerTitle: {
+    color: colors.WHITE,
+    fontFamily: fonts.BOLD,
+    paddingLeft: moderateScale(10),
+    paddingTop: moderateScale(-10),
+    paddingVertical: moderateScale(10),
+  },
+
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center',
+    backgroundColor: colors.BLACK,
+  },
+
+  button: {
+    backgroundColor: colors.BLACK,
+    paddingBottom: moderateScale(20),
   },
 });
 
