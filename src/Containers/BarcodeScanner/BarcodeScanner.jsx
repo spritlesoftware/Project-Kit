cc0f5f6da007cdd83a212777141dd94447149c92
sprintle <sprintle@sprintles-MacBook-Pro.local>
2023-11-25 22:24:21 +0530

    styles:enhanced UI

--- src/Containers/BarcodeScanner/BarcodeScanner.jsx
+++ src/Containers/BarcodeScanner/BarcodeScanner.jsx
@@ -1,59 +1,117 @@
 import * as React from 'react';
-import { StyleSheet, Text } from 'react-native';
-import { useCameraDevices } from 'react-native-vision-camera';
+import { StyleSheet, Text,View ,Pressable,Dimensions} from 'react-native';
+import { useCameraDevice , useCodeScanner} from 'react-native-vision-camera';
 import { Camera } from 'react-native-vision-camera';
-import { useScanBarcodes, BarcodeFormat } from 'vision-camera-code-scanner';
+import { transparent } from 'react-native-paper/lib/typescript/styles/themes/v2/colors';
+import {RNHole, RNHoleView} from "react-native-hole-view"
 
 export default function BarcodeScanner() {
+  const windowWidth = Dimensions.get('window').width;
+  const windowHeight = Dimensions.get('window').height;
+
   const [hasPermission, setHasPermission] = React.useState(false);
-  const devices = useCameraDevices();
-  const device = devices.back;
+  const device = useCameraDevice('back');
 
-  const [frameProcessor, barcodes] = useScanBarcodes([BarcodeFormat.QR_CODE], {
-    checkInverted: true,
-  });
-
-  // Alternatively you can use the underlying function:
-  //
-  // const frameProcessor = useFrameProcessor((frame) => {
-  //   'worklet';
-  //   const detectedBarcodes = scanBarcodes(frame, [BarcodeFormat.QR_CODE], { checkInverted: true });
-  //   runOnJS(setBarcodes)(detectedBarcodes);
-  // }, []);
+  const codeScanner = useCodeScanner({
+    codeTypes: ['qr', 'ean-13'],
+    onCodeScanned: (codes) => {
+      console.log(`Scanned ${JSON.stringify(codes[0].value)} codes!`)
+    }
+  })
 
   React.useEffect(() => {
     (async () => {
       const status = await Camera.requestCameraPermission();
-      setHasPermission(status === 'authorized');
+      console.log(status)
+      setHasPermission(status === 'granted');
     })();
   }, []);
-
+  console.log("hasPermission",hasPermission)
   return (
-    device != null &&
+    
     hasPermission && (
       <>
         <Camera
-          style={StyleSheet.absoluteFill}
+          style={{width:"100%",height:"100%"}}
           device={device}
           isActive={true}
-          frameProcessor={frameProcessor}
-          frameProcessorFps={5}
+          codeScanner={codeScanner}
         />
-        {barcodes.map((barcode, idx) => (
-          <Text key={idx} style={styles.barcodeTextURL}>
-            {barcode.displayValue}
-          </Text>
-        ))}
+
+      {/* Back Button */}
+      <RNHoleView
+                style={{
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%',
+                    backgroundColor: 'rgba(1,1,1,.7)'
+                }}
+                holes={[{x:windowWidth/4.5, y: windowHeight/3, width: 230, height: 230,borderRadius:25}]}
+            > 
+                <View style={{
+      width: 260,
+      height: 260,
+      // borderRadius: 20,
+      overflow: 'hidden', 
+      rowGap:100,
+      position:"absolute",
+      top:windowHeight/3.2,
+      left:windowWidth/5.5
+      // This is important to clip the border at the corners
+    }}>
+      <View style={{
+        flex: 1,
+        flexDirection: 'row',
+        columnGap:100
+      }}>
+        <View style={{ flex: 1,borderTopLeftRadius:35, borderTopWidth: 6, borderTopColor: '#00A1F1', borderLeftWidth: 6, borderLeftColor: '#00A1F1' }} />
+        <View style={{ flex: 1,borderTopRightRadius:35, borderTopWidth: 6, borderTopColor: '#7CBB00', borderRightWidth: 6, borderRightColor: '#7CBB00' }} />
+      </View>
+      <View style={{
+        flex: 1,
+        flexDirection: 'row',
+        columnGap:100
+      }}>
+        <View style={{ flex: 1,borderBottomLeftRadius:35, borderBottomWidth: 6, borderBottomColor: '#FFBB00', borderLeftWidth: 6, borderLeftColor: '#FFBB00' }} />
+        <View style={{ flex: 1,borderBottomRightRadius:35, borderBottomWidth: 6, borderBottomColor: '#F65314', borderRightWidth: 6, borderRightColor: '#F65314' }} />
+      </View>
+    </View>
+
+           
+        </RNHoleView>
       </>
-    )
-  );
+     )
+  )
 }
 
 const styles = StyleSheet.create({
   barcodeTextURL: {
     fontSize: 20,
     color: 'white',
-    fontWeight: 'bold',
+    fontWeight: 'bold',  
     backgroundColor: 'red',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  button: {
+    marginBottom:20,
+    width: 200,
+    height: 200,
+    borderRadius:100,
+    backgroundColor: 'rgba(140, 140, 140, 0.3)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  rightButtonRow: {
+    position: 'absolute',
+    right: 100,
+    top:200,
+  },
+  backButton: {
+    position: 'absolute',
+    left: 100,
+    top:200,
   },
 });
