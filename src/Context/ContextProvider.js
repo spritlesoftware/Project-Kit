ed624095ccd7892905c47c3477485b74a1f8716a
gowrishankar <gowrishankaroffl@gmail.com>
2023-11-27 11:08:30 +0530

    Feat: Audio Context Provider with updated states

--- src/Context/ContextProvider.js
+++ src/Context/ContextProvider.js
@@ -1,5 +1,4 @@
 import React, {createContext, useContext, useMemo, useState} from 'react';
-import {handleFavourites, handleItemPress} from '../Functions/Audio/AllSongs';
 
 const AppContext = createContext();
 
@@ -8,44 +7,22 @@ export const useAppContext = () => {
 };
 
 const ContextProvider = ({children}) => {
-  const [isPlayerReady, setIsPlayerReady] = useState(false);
   const [queue, setQueue] = useState([]);
   const [currentTrack, setCurrentTrack] = useState([]);
   const [favourites, setFavourites] = useState([]);
 
-  function HandleFavourites(item) {
-    handleFavourites(item, favourites, setFavourites);
-  }
-
-  function handlePress(index) {
-    handleItemPress(index);
-  }
-
-  const dataProvider = useMemo(() => {
+  const data = useMemo(() => {
     return {
-      isPlayerReady,
-      setIsPlayerReady,
       queue,
       setQueue,
       currentTrack,
       setCurrentTrack,
       favourites,
       setFavourites,
-      handleFavourites,
-      handlePress,
     };
-  }, [
-    isPlayerReady,
-    queue,
-    currentTrack,
-    favourites,
-    HandleFavourites,
-    handlePress,
-  ]);
+  }, [queue, currentTrack, favourites]);
 
-  return (
-    <AppContext.Provider value={dataProvider}>{children}</AppContext.Provider>
-  );
+  return <AppContext.Provider value={data}>{children}</AppContext.Provider>;
 };
 
 export default ContextProvider;
