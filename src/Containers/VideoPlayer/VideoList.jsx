9ee1c902683c3b2b9d158c917868b4bad13d3cac
gowrishankar <gowrishankaroffl@gmail.com>
2023-12-07 17:22:57 +0530

    Feat: Video List UI with bottom drawer

--- src/Containers/VideoPlayer/VideoList.jsx
+++ src/Containers/VideoPlayer/VideoList.jsx
@@ -1,15 +1,52 @@
-import {View, Text, StyleSheet, FlatList} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Animated,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import React, {useState} from 'react';
 import {colors} from '../../Utils/colors';
 import List from '../../Components/Video/List';
 import HeaderWithBackaction from '../../Components/Header/HeaderWithBackaction';
 import {videoData} from '../../Data/VideoData';
+import BottomDrawer from '../../Components/Drawer/BottomDrawer';
 
 const VideoList = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const drawerAnimation = new Animated.Value(0);
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+
+    Animated.timing(drawerAnimation, {
+      toValue: drawerOpen ? 0 : 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
     <View style={styles.container}>
       <HeaderWithBackaction title="Videos" />
-      <FlatList data={videoData} renderItem={item => <List item={item} />} />
+      <TouchableWithoutFeedback onPress={() => handleDrawerToggle()}>
+        <FlatList
+          data={videoData}
+          renderItem={item => (
+            <List
+              item={item}
+              handleDrawerToggle={handleDrawerToggle}
+              drawerOpen={drawerOpen}
+            />
+          )}
+        />
+      </TouchableWithoutFeedback>
+      <BottomDrawer
+        status={drawerOpen}
+        handleDrawerToggle={handleDrawerToggle}
+      />
     </View>
   );
 };
