f699ac2faed9dc5e8f91b84f6c6cb06e61ceb592
gowrishankar <gowrishankaroffl@gmail.com>
2023-12-14 12:57:32 +0530

    Updated: Added StatusBar to the whole app

--- App.jsx
+++ App.jsx
@@ -2,12 +2,18 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNavigator, {AppContext} from './src/Navigations/StackNavigator';
 import ContextProvider from './src/Context/ContextProvider';
+import {StatusBar} from 'react-native';
+import {colors} from './src/Utils/colors';
 
 function App() {
   // Used when generating any kind of tokens
   // To set up environmental variables, see http://twil.io/secure
   return (
     <ContextProvider>
+      <StatusBar
+        backgroundColor={colors.APP_PRIMARY}
+        barStyle="light-content"
+      />
       <NavigationContainer>
         <StackNavigator />
       </NavigationContainer>
