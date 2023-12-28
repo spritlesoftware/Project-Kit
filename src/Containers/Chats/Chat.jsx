45f9f310df6ae14dbbc449f27a56b98a2f2bb2af
gowrishankar <gowrishankaroffl@gmail.com>
2023-12-28 18:03:55 +0530

    Feat: Voice Message recorder & player in chat bubble: Completed

--- src/Containers/Chats/Chat.jsx
+++ src/Containers/Chats/Chat.jsx
@@ -1,25 +1,25 @@
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Image,
-  Button,
-} from 'react-native';
-import React, {useCallback, useEffect, useLayoutEffect, useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
 import {colors} from '../../Utils/colors';
 import {GiftedChat, Bubble, Send, InputToolbar} from 'react-native-gifted-chat';
 import HeaderWithBackaction from '../../Components/Header/HeaderWithBackaction';
-import {user_1, user_2, user_3} from '../../Data/ChatRoom';
 import {useRoute} from '@react-navigation/native';
 import {Icon} from 'react-native-paper';
 import {moderateScale} from 'react-native-size-matters';
-import DocumentPicker from 'react-native-document-picker';
 import FileTransfer from '../../Components/Chat/FileTransfer';
 import ViewFile from '../../Components/Chat/ViewFile';
 import Plus from 'react-native-vector-icons/AntDesign';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import ChatLogic from '../../Functions/Chat/Chat';
+import Mic from 'react-native-vector-icons/Entypo';
+import {fonts} from '../../Utils/fonts';
+import PlayPause from 'react-native-vector-icons/AntDesign';
+import Delete from 'react-native-vector-icons/MaterialCommunityIcons';
+import Slider from '@react-native-community/slider';
+import TrackPlayer, {useProgress} from 'react-native-track-player';
+import {usePlaybackState} from 'react-native-track-player';
+import _ from 'lodash';
+import Wave from 'react-native-vector-icons/MaterialIcons';
 
 const Chat = () => {
   const {
@@ -36,20 +36,104 @@ const Chat = () => {
     pickDocument,
     navigation,
     currentTime,
+    setAudioURL,
+    audioURL,
+    recordingActive,
+    setRecordingActive,
+    time,
+    setTime,
+    StartRecording,
+    StopRecording,
+    DeleteRecording,
+    isRecordingPaused,
+    toggleRecording,
+    playingAudio,
+    setPlayingAudio,
+    Format,
   } = ChatLogic();
+
+  // voice message player
+  const {position, duration} = useProgress(0);
+
+  const [currentPositionSec, setCurrentPositionSec] = useState(0);
+  const [currentDurationSec, setCurrentDurationSec] = useState(null);
+  const [currentAudioId, setCurrentAudioId] = useState(null);
+
+  const {state} = usePlaybackState();
+
+  useEffect(() => {
+    TrackPlayer.setupPlayer();
+  }, []);
+
+  useEffect(() => {
+    // console.log(position, duration, ' ]]]]');
+    setCurrentPositionSec(position);
+    setCurrentDurationSec(duration);
+
+    if (position == duration) {
+      TrackPlayer.seekTo(0);
+      TrackPlayer.pause();
+      setPlayingAudio(false);
+    }
+  }, [position, duration]);
+
+  const playAudio = message => {
+    TrackPlayer.add({
+      id: message._id,
+      url: message.audio.url,
+      title: message.text,
+    });
+    TrackPlayer.play();
+    setPlayingAudio(true);
+    setCurrentAudioId(message._id);
+  };
+
+  const TogglePlayback = message => {
+    if (state === 'playing') {
+      TrackPlayer.pause();
+      setPlayingAudio(false);
+      setCurrentAudioId(null);
+    } else {
+      playAudio(message);
+    }
+  };
+
+  const onSliderValueChange = useCallback(
+    _.debounce(value => {
+      TrackPlayer.seekTo(value);
+    }, 300), // Adjust the debounce delay as needed
+    [],
+  );
 
   const route = useRoute();
 
   const renderSend = props => {
     return (
       <View style={styles.shareContainer}>
-        <TouchableOpacity
-          onPress={attachments.length <= 3 ? pickDocument : null}
-          style={styles.fileShare}>
-          <Plus name="pluscircle" color={colors.BLACK} size={25} />
-        </TouchableOpacity>
-        <Send {...props}>
-          <Icon source="send" size={25} color={colors.BLACK} />
+        {!recordingActive && (
+          <TouchableOpacity
+            onPress={attachments.length <= 3 ? pickDocument : null}
+            style={styles.fileShare}>
+            <Plus
+              name="pluscircle"
+              color={colors.APP_PRIMARY}
+              size={25}
+              style={{
+                paddingTop: moderateScale(1),
+                paddingRight: moderateScale(1),
+                borderWidth: 1,
+                borderColor: colors.TRANSPARENT,
+                alignSelf: 'center',
+              }}
+            />
+          </TouchableOpacity>
+        )}
+        <Send
+          {...props}
+          containerStyle={{
+            marginRight: moderateScale(50),
+          }}>
+          <Icon source="send" size={25} color={colors.APP_PRIMARY} />
         </Send>
       </View>
     );
@@ -57,13 +141,16 @@ const Chat = () => {
 
   const renderBubble = props => {
     const {currentMessage} = props;
+
     if (currentMessage.file && currentMessage.file.url) {
       return (
         <TouchableOpacity
           style={{
             ...styles.fileContainer,
             backgroundColor:
-              props.currentMessage.user._id === 2 ? '#2e64e5' : '#efefef',
+              props.currentMessage.user._id === 2
+                ? colors.APP_PRIMARY
+                : '#efefef',
             borderBottomLeftRadius:
               props.currentMessage.user._id === 2 ? 15 : 5,
             borderBottomRightRadius:
@@ -97,13 +184,69 @@ const Chat = () => {
           </View>
         </TouchableOpacity>
       );
+    } else if (currentMessage.audio && currentMessage.audio.url) {
+      const audioDurationString = currentMessage.audio.duration || '0:00';
+      const [minutes, seconds] = audioDurationString.split(':');
+      const audioDuration = parseInt(minutes, 10) * 60 + parseInt(seconds, 10);
+      // console.log(audioDuration, ' 00');
+      return (
+        <TouchableOpacity
+          style={{
+            ...styles.audioBubble,
+            backgroundColor:
+              props.currentMessage.user._id === 2
+                ? colors.APP_PRIMARY
+                : '#efefef',
+            borderBottomLeftRadius:
+              props.currentMessage.user._id === 2 ? 15 : 5,
+            borderBottomRightRadius:
+              props.currentMessage.user._id === 2 ? 5 : 15,
+            borderTopLeftRadius: props.currentMessage.user._id === 2 ? 15 : 5,
+          }}>
+          <TouchableOpacity
+            style={{
+              width: moderateScale(30),
+            }}
+            onPress={() => TogglePlayback(currentMessage)}>
+            <PlayPause
+              name={
+                currentAudioId === currentMessage._id && playingAudio
+                  ? 'pausecircle'
+                  : 'play'
+              }
+              color={colors.WHITE}
+              size={25}
+              style={styles.PlayPauseButton}
+            />
+          </TouchableOpacity>
+          <Slider
+            style={{flex: 1}}
+            value={currentAudioId === currentMessage._id ? position : 0}
+            minimumValue={0}
+            maximumValue={duration}
+            onValueChange={
+              currentAudioId === currentMessage._id && onSliderValueChange
+            }
+            minimumTrackTintColor={colors.WHITE}
+            maximumTrackTintColor={colors.WHITE}
+            thumbTintColor={colors.WHITE}
+          />
+          <View>
+            <Text style={styles.audioText}>
+              {currentAudioId === currentMessage._id && playingAudio
+                ? Format(position)
+                : currentMessage.audio.duration}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      );
     }
     return (
       <Bubble
         {...props}
         wrapperStyle={{
           right: {
-            backgroundColor: '#2e64e5',
+            backgroundColor: colors.APP_PRIMARY,
           },
         }}
         textStyle={{
@@ -123,8 +266,33 @@ const Chat = () => {
     const modifiedProps = {...props};
     if (props.text.length === 0 && attachments[0] !== undefined) {
       modifiedProps.text = ' ';
+    } else if (audioURL !== '') {
+      modifiedProps.text = ' ';
     }
-    return <InputToolbar {...modifiedProps} containerStyle={styles.input} />;
+    return (
+      <View style={styles.inputWrapper}>
+        <InputToolbar {...modifiedProps} containerStyle={styles.input} />
+        <TouchableOpacity
+          style={[
+            styles.microphone,
+            {
+              backgroundColor: recordingActive
+                ? colors.GREY11
+                : colors.APP_PRIMARY,
+              borderWidth: 0,
+            },
+          ]}
+          activeOpacity={recordingActive ? 1 : 0}
+          onLongPress={!recordingActive && StartRecording}>
+          <Mic
+            name="mic"
+            color={colors.WHITE}
+            size={20}
+            style={{padding: moderateScale(5)}}
+          />
+        </TouchableOpacity>
+      </View>
+    );
   };
 
   const renderChatFooter = useCallback(() => {
@@ -181,10 +349,39 @@ const Chat = () => {
           ))}
         </View>
       );
-    } else {
-      return null;
+    } else if (recordingActive) {
+      return (
+        <View style={styles.audioFooter}>
+          <View style={styles.audioInnerContainer}>
+            <Wave
+              name="multitrack-audio"
+              color={colors.APP_PRIMARY}
+              size={moderateScale(20)}
+            />
+            <Text style={styles.recorderText}>
+              Recording Voice {'  '} {time}
+            </Text>
+          </View>
+          {/* <TouchableOpacity onPress={() => toggleRecording()}>
+            <PlayPause
+              name={isRecordingPaused ? 'playcircleo' : 'pause'}
+              color={colors.APP_PRIMARY}
+              size={moderateScale(25)}
+              style={styles.PlayPauseButton}
+            />
+          </TouchableOpacity> */}
+          <TouchableOpacity
+            onPress={() => audioURL !== '' && DeleteRecording()}>
+            <Delete
+              name="delete"
+              color={colors.APP_PRIMARY}
+              size={moderateScale(25)}
+            />
+          </TouchableOpacity>
+        </View>
+      );
     }
-  }, [attachments]);
+  }, [attachments, time, recordingActive, audioURL]);
 
   return (
     <View style={styles.container}>
@@ -208,6 +405,7 @@ const Chat = () => {
         renderChatFooter={renderChatFooter}
         renderInputToolbar={renderInputToolbar}
         messagesContainerStyle={styles.messagesContainer}
+        placeholder="Say something..."
       />
     </View>
   );
@@ -222,8 +420,7 @@ const styles = StyleSheet.create({
   shareContainer: {
     flexDirection: 'row',
     marginBottom: 12,
-    marginRight: moderateScale(10),
-    width: moderateScale(70),
+    marginRight: moderateScale(-40),
     height: moderateScale(40),
     justifyContent: 'space-between',
   },
@@ -298,6 +495,13 @@ const styles = StyleSheet.create({
 
   fileShare: {
     marginTop: moderateScale(16),
+    marginRight: moderateScale(10),
+    borderWidth: 1,
+    borderColor: colors.TRANSPARENT,
+    width: moderateScale(40),
+    height: moderateScale(30),
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
 
   timeText: {
@@ -310,15 +514,102 @@ const styles = StyleSheet.create({
   input: {
     borderRadius: moderateScale(30),
     backgroundColor: colors.GRAY10,
-    marginBottom: moderateScale(10),
     borderTopWidth: 0,
     marginHorizontal: moderateScale(10),
-    marginRight: moderateScale(4),
+    marginRight: moderateScale(60),
     alignItems: 'center',
+    marginBottom: moderateScale(5),
+    width: moderateScale(280),
   },
 
   messagesContainer: {
     paddingBottom: moderateScale(15),
+    fontFamily: fonts.MEDIUM,
+  },
+
+  microphone: {
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    marginRight: moderateScale(10),
+    marginTop: moderateScale(-5),
+    backgroundColor: colors.APP_PRIMARY,
+    width: moderateScale(50),
+    height: moderateScale(52),
+    borderRadius: moderateScale(30),
+    bottom: moderateScale(5),
+  },
+
+  inputWrapper: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    bottom: moderateScale(10),
+  },
+
+  recordContainer: {
+    width: moderateScale(300),
+    height: moderateScale(50),
+    borderRadius: moderateScale(30),
+    backgroundColor: colors.GRAY10,
+    alignSelf: 'flex-start',
+    justifyContent: 'center',
+    marginHorizontal: moderateScale(10),
+    bottom: moderateScale(15),
+    paddingHorizontal: moderateScale(15),
+  },
+
+  audioFooter: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    elevation: 2,
+    borderBottomLeftRadius: moderateScale(10),
+    borderBottomRightRadius: moderateScale(10),
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderWidth: 1,
+    borderColor: 'rgba(255, 255, 255, 0.18)',
+    height: moderateScale(60),
+    marginVertical: moderateScale(10),
+    marginHorizontal: moderateScale(10),
+    paddingTop: moderateScale(5),
+    paddingHorizontal: moderateScale(10),
+  },
+
+  recorderText: {
+    fontFamily: fonts.BOLD,
+    fontSize: moderateScale(14),
+    marginHorizontal: moderateScale(10),
+  },
+
+  audioInnerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  audioBubble: {
+    flexDirection: 'row',
+    padding: moderateScale(10),
+    justifyContent: 'center',
+    alignItems: 'center',
+    maxWidth: moderateScale(250),
+  },
+
+  audioText: {
+    fontFamily: fonts.BOLD,
+    color: colors.WHITE,
+    paddingHorizontal: moderateScale(10),
+  },
+
+  sliderContainer: {
+    paddingHorizontal: moderateScale(5),
+    marginBottom: moderateScale(10),
+  },
+
+  PlayPauseButton: {
+    paddingTop: moderateScale(1),
+    paddingHorizontal: moderateScale(1),
   },
 });
 
